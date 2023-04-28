89cae529db81624e1e391a0a18db8028806042f0
Amit Gupta <amit.the.coder404@gmail.com>
2023-04-28 21:59:41 +0530

    submitting code-Fri Apr 28 2023 21:59:39 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import './../styles/App.css';
+import React from "./Botton";
 
 const App = (props) => {
 
   return (
     <div className="App" id="main">
       // Do not alter the main div
+      <Button/>
     </div>
   );
 }
